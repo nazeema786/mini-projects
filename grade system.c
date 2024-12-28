410db346581acb41d1956d36e85b2e251e53deab
nazeema786 <shaiksnazeema@gmail.com>
2024-12-28 18:08:54 +0530

    Create grade system.c

---
+++ grade system.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+  int n;
+  float credits,grade_points,total_credits=0,total_grade_points=0;
+  float cgpa;
+  printf("Enter the number of courses:");
+  scanf("%d",&n);
+  for(int i=0;i<n;i++)
+  {
+    printf("Enter credits for course %d: ",i+1);
+    scanf("%d",&credits);
+    printf("Enter grade points for course %d: ",i+1);
+    scanf("%f",&grade_points);
+    total_credits+=credits;
+    total_grade_points+=(grade_points*credits);
+  }
+  cgpa=total_grade_points/total_credits;
+  printf("CGPA: %.2f\n",cgpa);
+  return 0;
+}
+
